bbdc82e700a7bc443a747f45d79d1dbac47c1647
Renoir Boulanger <Renoir.Boulanger@logmein.com>
2021-08-02 12:26:42 -0400

    Honk

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,26 @@
-/**
- * Boilerplate ESLint config, adjust to your requirements
- * See: https://www.npmjs.com/package/@renoirb/conventions-use-eslint
- */
-const base = require('@renoirb/conventions-use-eslint')
+// This is a workaround for https://github.com/eslint/eslint/issues/3458
+require('@rushstack/eslint-config/patch/modern-module-resolution')
 
 /**
+ * Bookmarks:
+ * - https://rushstack.io/pages/heft_tasks/eslint/
+ * - https://www.npmjs.com/package/@rushstack/eslint-config
+ *
  * @type {import('@types/eslint').Linter.Config}
  */
-const main = {
-  ...base,
-  root: true,
-  env: {
-    node: true,
-  },
+module.exports = {
   parserOptions: {
-    parser: 'babel-eslint',
-    tsconfigRootDir: process.cwd(),
-    project: './tsconfig.json',
-    ...(base.parserOptions || {}),
+    tsconfigRootDir: __dirname,
   },
+  extends: ['@rushstack/eslint-config/profile/web-app'],
   rules: {
-    ...base.rules,
     // TODO: Make this list smaller, not bigger
+    '@rushstack/typedef-var': 'off',
+    '@typescript-eslint/consistent-type-definitions': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-member-accessibility': 'off',
+    '@typescript-eslint/naming-convention': 'off',
+    '@typescript-eslint/no-parameter-properties': 'off',
+    '@typescript-eslint/typedef': 'off',
   },
 }
-
-module.exports = main
